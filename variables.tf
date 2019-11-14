7e3a8a41755f03ba550296572434a6a9724dc3b2
ManojChandran <manoj.meparmbu@gmail.com>
2019-11-14 08:50:31 -0500

    main with backend

--- variables.tf
+++ variables.tf
@@ -5,6 +5,10 @@ variable "aws-region" {
   default     = "us-east-1"
 }
 
+variable "env" {
+  description = "env: prod or test or dev"
+}
+
 variable "vpc-cidr" {
   description = "stores ip cidr for the VPC"
   default     = "10.23.0.0/16"
