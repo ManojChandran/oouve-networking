5bfd33b226ed3b2cc2c7b19b3b9283def15867ea
ManojChandran <manoj.meparmbu@gmail.com>
2019-11-20 11:57:34 -0500

    added ingress and egress in NACL

--- modules/15_nacl/main.tf
+++ modules/15_nacl/main.tf
@@ -21,22 +21,76 @@ variable "vpc-cidr" {}
 resource "aws_network_acl" "oouve-nacl" {
   vpc_id  = "${var.vpc-id}"
 
-  egress {
-    protocol   = "tcp"
-    rule_no    = 200
-    action     = "allow"
-    cidr_block = "${var.vpc-cidr}"
-    from_port  = 443
-    to_port    = 443
+  ingress {
+    protocol = "tcp"
+    rule_no = 100
+    action = "allow"
+    cidr_block =  "0.0.0.0/0"
+    from_port = 80
+    to_port = 80
   }
 
   ingress {
-    protocol   = "tcp"
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "${var.vpc-cidr}"
-    from_port  = 80
-    to_port    = 80
+    protocol = "tcp"
+    rule_no = 101
+    action = "allow"
+    cidr_block =  "0.0.0.0/0"
+    from_port = 443
+    to_port = 443
+  }
+
+  ingress {
+    protocol = "tcp"
+    rule_no = 102
+    action = "allow"
+    cidr_block =  "0.0.0.0/0"
+    from_port = 22
+    to_port = 22
+  }
+
+  ingress {
+    protocol = "tcp"
+    rule_no = 103
+    action = "allow"
+    cidr_block =  "0.0.0.0/0"
+    from_port = 1024
+    to_port = 65535
+  }
+
+#  egress {
+#    protocol = "all"
+#    rule_no = 100
+#    action = "allow"
+#    cidr_block =  "0.0.0.0/0"
+#    from_port = 0
+#    to_port = 65535
+#  }
+
+  egress {
+    protocol = "tcp"
+    rule_no = 101
+    action = "allow"
+    cidr_block =  "0.0.0.0/0"
+    from_port = 80
+    to_port = 80
+  }
+
+  egress {
+    protocol = "tcp"
+    rule_no = 102
+    action = "allow"
+    cidr_block =  "0.0.0.0/0"
+    from_port = 443
+    to_port = 443
+  }
+
+  egress {
+    protocol = "tcp"
+    rule_no = 103
+    action = "allow"
+    cidr_block =  "0.0.0.0/0"
+    from_port = 1024
+    to_port = 65535
   }
 
   tags = {
