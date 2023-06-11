b68ff1c2bb16c58d15fab471230e8d7b70d3a440
Manoj Chandran <manoj.meparambu@gmail.com>
2023-06-11 18:59:27 +0530

    update asg

--- modules/21_asg_web/mian.tf
+++ modules/21_asg_web/mian.tf
@@ -25,6 +25,10 @@ resource "aws_launch_configuration" "web-lc" {
 
 resource "aws_autoscaling_group" "web-asg" {
   launch_configuration = "${aws_launch_configuration.web-lc.id}"
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 4
+  force_delete              = true
   max_size = 5
   min_size = 2  
 }
